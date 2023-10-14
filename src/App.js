a1c537f54c42358a5ab3b9fa834644a9e6ab01aa
vikhulei <65869915+vikhulei@users.noreply.github.com>
2023-10-14 12:17:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My Learning Platform</h1>
+      <h1>Yes, this is My Learning Platform</h1>
     </div>
   );
 }
