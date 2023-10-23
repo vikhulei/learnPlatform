6d5f49d91ee4af5f10c05b0e170c4901c0b17535
vikhulei <vikhulei@gmail.com>
2023-10-23 18:09:49 +0100

    claculation started

--- src/apps/calculator/Calculator.js
+++ src/apps/calculator/Calculator.js
@@ -1,15 +1,26 @@
+import { useState } from "react"
 import { Frame, Output, Result, Calculation, ButtonsWraper, Button, BlackButton, ButtonEquals } from "./CalculatorStyle"
 
+
+
 const Calculator = () => {
+
+    const [prevInput, setPrevInput] = useState("")
+    const [input, setInput] = useState("")
+
+    const test = (e) => {
+        let numberKey = +e.target.innerHTML
+        if (isNaN(numberKey) === false) {
+            setPrevInput(+prevInput+(+e.target.innerHTML));
+        }
+        setInput(input+(e.target.innerHTML))
+    }
+
     return (
         <Frame>
             <Output>
-                <Result>
-                    <p>4</p>
-                </Result>
-                <Calculation>
-                    <p>2+2</p>
-                </Calculation>
+                <Result>{prevInput}</Result>
+                <Calculation>{input}</Calculation>
             </Output>
             <ButtonsWraper>
                 <BlackButton>M+</BlackButton>
@@ -21,12 +32,12 @@ const Calculator = () => {
                 <Button>&#40;</Button>
                 <Button>&#41;</Button>
                 <Button>%</Button>
-                <Button red>C</Button>
-                <Button>7</Button>
-                <Button>8</Button>
+                <Button red="true" onClick={test}>C</Button>
+                <Button onClick={test}>7</Button>
+                <Button onClick={test}>8</Button>
                 <Button>9</Button>
                 <Button>÷</Button>
-                <Button orange>&lt;=</Button>
+                <Button orange="true">&lt;=</Button>
                 <Button>4</Button>
                 <Button>5</Button>
                 <Button>6</Button>
@@ -39,7 +50,7 @@ const Calculator = () => {
                 <Button>E</Button>
                 <Button>0</Button>
                 <Button>.</Button>
-                <Button>+</Button>
+                <Button onClick={test}>+</Button>
             </ButtonsWraper>
         </Frame>
     )
