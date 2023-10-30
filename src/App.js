6de2f95ae0d824a12fbcba9acf55ef8275668cf4
vikhulei <vikhulei@gmail.com>
2023-10-30 07:48:18 +0000

    Router started

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { BodyWrapper, NavbarWrapper, MainWrapper } from "./components/ui/StyledComponents"
 import Navbar from './components/navbar/Navbar';
 import Home from './apps/pages/01_home/Home';
@@ -8,8 +9,13 @@ import Home from './apps/pages/01_home/Home';
 function App() {
   return (
     <BodyWrapper>
+      <Router>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/home" element={<Home/>} />
+      </Routes>
+      </Router>
     </BodyWrapper>
 
   );
